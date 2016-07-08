b8d671441d55d7882a5a39065a217f9aa1f88495
Isaac Schemm <isaacschemm@gmail.com>
2016-07-08 10:12:48 -0500

    probably not going to pursue this any further - broken right now

--- build.sh
+++ build.sh
@@ -1,10 +1,43 @@
 #!/bin/sh
+set -e
 if [ ! -f vmlinuz ];then
 	wget -O vmlinuz http://tinycorelinux.net/7.x/x86/release/distribution_files/vmlinuz
 fi
 if [ ! -f core.gz ];then
 	wget -O core.gz http://tinycorelinux.net/7.x/x86/release/distribution_files/core.gz
 fi
+if [ ! -d xbase ];then
+	mkdir xbase
+	for i in Xvesa Xlibs Xprogs aterm flwm_topside wbar;do
+		./get-tcz.sh $i xbase
+	done
+fi
 for i in $*;do
-
+	if [ -f $i.gz ];then
+		echo "$i.gz exists."
+	else
+		if [ -d $i ];then
+			rm $i/*.tcz
+			rmdir $i
+		fi
+		mkdir $i
+		cp xbase/*.tcz $i
+		./get-tcz.sh $i $i
+		
+		if [ -d initroot ];then
+			sudo rm -r initroot
+		fi
+		mkdir initroot
+		
+		(cd initroot; gzip -cd ../core.gz | sudo cpio -i -H newc -d)
+		for j in $i/*.tcz;do
+			sudo unsquashfs -d sq $j
+			sudo cp -r sq/* initroot
+			sudo rm -r sq
+		done
+		if [ $i = "gparted" ];then
+			(cd initroot/etc/init.d;sudo patch -p0 < ${FDIR}/tc-config.diff)
+		fi
+		(cd initroot; sudo find | sudo cpio -o -H newc | gzip > ../$i.gz)
+	fi
 done
--- get-tcz.sh
+++ get-tcz.sh
@@ -2,20 +2,20 @@
 set -e
 i=$1
 echo $0 $*
-if [ ! -d $2 ];then
+if [ "$2" = "" ] || [ ! -d $2 ];then
 	echo "Usage: $0 [filename].tcz [directory]"
 	return 1
 fi
 if !(echo $i | grep -q '\.tcz$');then
 	i=$i.tcz
 fi
+echo "Looking up dependencies for $i"
+wget -qO- http://distro.ibiblio.org/tinycorelinux/7.x/x86/tcz/$i.dep | while read j;do
+	if [ "$j" != "" ];then
+		$0 $j $2
+	fi
+done
 if [ ! -f $i ];then
-	echo "Looking up dependencies for $i"
-	wget -qO- http://distro.ibiblio.org/tinycorelinux/7.x/x86/tcz/$i.dep | while read j;do
-		if [ "$j" != "" ];then
-			$0 $j $2
-		fi
-	done
 	echo "Downloading $i"
 	wget -qO $i http://distro.ibiblio.org/tinycorelinux/7.x/x86/tcz/$i &
 	wait $!
